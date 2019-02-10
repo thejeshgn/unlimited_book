94fe05fe59314206f293c8ba3c36ccee3b252306
Thejesh GN <i@thejeshgn.com>
2019-02-10 21:34:22 +0530

    initial

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask import render_template
+
+import sqlite3
+from sqlite3 import Error
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/help')
+def help():
+    return render_template('help.html')
